cdebc23c33650d4b4880067d6706b2bfc4915e99
Esther Sánchez <sanchez.ther@gmail.com>
2018-05-09 19:02:27 +0200

    2017 day02a add do design

--- 2017/day02a/do.go
+++ 2017/day02a/do.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func do(input string) (int, error) {
-	return 12, fmt.Errorf("TO DO")
+	ss, err := parse(input)
+	if err != nil {
+		return 0, fmt.Errorf("parsing input: %v", err)
+	}
+	var sum int
+	for _, s := range ss {
+		sum += max(s) - min(s)
+	}
+	return sum, nil
+}
+
+// returns a error if s has:
+// - non-integers
+// - negative integers
+// - empty lines
+func parse(s string) ([][]int, error) {
+	return [][]int{{}}, nil
+}
+
+// Assumes len(s)>0
+func min(s []int) int {
+	return 0
+}
+
+// Assumes len(s)>0
+func max(s []int) int {
+	return 0
 }
