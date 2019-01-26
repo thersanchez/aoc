d7617ae08c7c825a870176996a10340ae1b80bfb
Alberto Cortés <alcortesm@gmail.com>
2019-01-26 12:44:35 +0100

    2017 day05a implement mem.NewFromReader instead of load, missing tests

--- 2017/day05a/do_test.go
+++ 2017/day05a/do_test.go
@@ -13,18 +13,40 @@ func TestDo(t *testing.T) {
 		want  int
 	}{
 		{
-			desc:  "TODO",
-			input: "1\n0",
+			desc:  "empty memory",
+			input: "",
+			want:  0,
+		},
+		{
+			desc:  "1 jump",
+			input: "1",
 			want:  1,
+		},
+		{
+			desc:  "jump forth and back",
+			input: "1\n-1",
+			want:  3,
 		},
 	} {
 		test := test
 		t.Run(test.desc, func(t *testing.T) {
 			t.Parallel()
-			got := do(strings.NewReader(test.input))
+			got, err := do(strings.NewReader(test.input))
+			if err != nil {
+				t.Fatal(err)
+			}
 			if got != test.want {
 				t.Errorf("want %d, got %d", test.want, got)
 			}
 		})
+	}
+}
+
+func TestDoError(t *testing.T) {
+	t.Parallel()
+	input := "1\n3\nerror\n5"
+	_, err := do(strings.NewReader(input))
+	if err == nil {
+		t.Errorf("unexpected success")
 	}
 }
