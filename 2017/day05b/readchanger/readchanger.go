21634da82b6c9b42e458b796bc691439207c0486
Esther Sánchez <sanchez.ther@gmail.com>
2019-10-27 19:05:10 +0100

    2017 day05b readchanger

--- 2017/day05b/readchanger/readchanger.go
+++ 2017/day05b/readchanger/readchanger.go
@@ -1,27 +1,43 @@
 package readchanger
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// ReadChanger can read memories and change its contets based on the
+// value being read (See ReadAndChange).
 type ReadChanger struct {
 	rw ReadWriter
 }
 
+// ReadWriter describes a memory from the point of view of a ReadChanger.
 type ReadWriter interface {
 	Read(addr int) (int, error)
 	Write(addr, value int) error
 }
 
-func NewReadChanger(m ReadWriter) *ReadChanger {
+// New returns a ReadChanger using m as its memory.
+func New(m ReadWriter) *ReadChanger {
 	return &ReadChanger{rw: m}
 }
 
+// ReadAndChange reads the memory address addr and returns its contents.
+// Also changes the value stored there in two different ways:
+// - Adds 1 if the value was less than 3.
+// - Substracts 1 otherwise.
+// Returns an error if addr is out memory bounds.
 func (rc *ReadChanger) ReadAndChange(addr int) (int, error) {
 	v, err := rc.rw.Read(addr)
 	if err != nil {
 		return 0, fmt.Errorf("reading: %v", err)
 	}
-	// TODO substract 3 when needed
-	err = rc.rw.Write(addr, v+1)
+
+	newValue := v - 1
+	if v < 3 {
+		newValue = v + 1
+	}
+
+	err = rc.rw.Write(addr, newValue)
 	if err != nil {
 		return 0, fmt.Errorf("writing: %v", err)
 	}
--- 2017/day05b/readchanger/readchanger_test.go
+++ 2017/day05b/readchanger/readchanger_test.go
@@ -2,6 +2,7 @@ package readchanger_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/thersanchez/aoc/2017/day05b/readchanger"
@@ -21,12 +22,13 @@ func (m mockMem) Write(addr int, value int) error {
 }
 
 func TestCanHandleReadError(t *testing.T) {
+	t.Parallel()
 	mm := mockMem{
 		read: func(addr int) (int, error) {
 			return 0, errors.New("some read error")
 		},
 	}
-	rc := readchanger.NewReadChanger(mm)
+	rc := readchanger.New(mm)
 	_, err := rc.ReadAndChange(10)
 	if err == nil {
 		t.Errorf("unexpected success")
@@ -34,6 +36,7 @@ func TestCanHandleReadError(t *testing.T) {
 }
 
 func TestCanHandleWriteError(t *testing.T) {
+	t.Parallel()
 	mm := mockMem{
 		read: func(addr int) (int, error) {
 			return 42, nil
@@ -42,7 +45,7 @@ func TestCanHandleWriteError(t *testing.T) {
 			return errors.New("some write error")
 		},
 	}
-	rc := readchanger.NewReadChanger(mm)
+	rc := readchanger.New(mm)
 	_, err := rc.ReadAndChange(10)
 	if err == nil {
 		t.Errorf("unexpected success")
@@ -50,6 +53,7 @@ func TestCanHandleWriteError(t *testing.T) {
 }
 
 func TestReadsCorrectly(t *testing.T) {
+	t.Parallel()
 	want := struct {
 		addr  int
 		value int
@@ -58,8 +62,13 @@ func TestReadsCorrectly(t *testing.T) {
 		value: 10,
 	}
 
+	// a memory that complains if it is asked to read
+	// from the wrong address.
+	// It also counts the number of times it is read in nReads.
+	nReads := 0
 	mm := mockMem{
 		read: func(addr int) (int, error) {
+			nReads++
 			if addr != want.addr {
 				t.Errorf("wrong addr passed to mem, want %d, got %d",
 					want.addr, addr)
@@ -69,12 +78,69 @@ func TestReadsCorrectly(t *testing.T) {
 		write: func(addr, value int) error { return nil },
 	}
 
-	rc := readchanger.NewReadChanger(mm)
+	rc := readchanger.New(mm)
 	got, err := rc.ReadAndChange(want.addr)
 	if err != nil {
 		t.Error(err)
 	}
 	if got != want.value {
 		t.Errorf("want %d, got %d", want.value, got)
+	}
+	if nReads != 1 {
+		t.Errorf("wrong number of read calls: want 1, got %d", nReads)
+	}
+}
+
+func TestChangesCorrectly(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		addr     int
+		original int
+		changed  int
+	}{
+		{addr: 10, original: 0, changed: 1},
+		{addr: 11, original: 1, changed: 2},
+		{addr: 12, original: 2, changed: 3},
+		{addr: 13, original: 3, changed: 2},
+		{addr: 14, original: 4, changed: 3},
+		{addr: 15, original: 5, changed: 4},
+		{addr: 16, original: 6, changed: 5},
+		{addr: 17, original: 7, changed: 6},
+	}
+
+	for _, test := range tests {
+		desc := fmt.Sprintf("%d", test.original)
+		test := test
+		t.Run(desc, func(t *testing.T) {
+			t.Parallel()
+			// a memory that complains if it is asked to write
+			// the wrong value or on the wrong address.
+			// It also counts the number of times it is written in nWrites.
+			nWrites := 0
+			mm := mockMem{
+				read: func(_ int) (int, error) {
+					return test.original, nil
+				},
+				write: func(addr, value int) error {
+					nWrites++
+					if addr != test.addr {
+						t.Errorf("wrong addr: want %d, got %d", test.addr, addr)
+					}
+					if value != test.changed {
+						t.Errorf("wrong value: want %d, got %d", test.changed, value)
+					}
+					return nil
+				},
+			}
+
+			rc := readchanger.New(mm)
+			_, err := rc.ReadAndChange(test.addr)
+			if err != nil {
+				t.Error(err)
+			}
+			if nWrites != 1 {
+				t.Errorf("wrong number of write calls: want 1, got %d", nWrites)
+			}
+		})
 	}
 }
