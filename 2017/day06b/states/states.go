64a21c1db65ea72f54323d44baf74a57c38f7a8d
Esther Sánchez <sanchez.ther@gmail.com>
2020-10-12 15:05:03 +0200

    2017 day06b - inject mem to states through interface

--- 2017/day06b/states/states.go
+++ 2017/day06b/states/states.go
@@ -1,10 +1,13 @@
 package states
 
-import "github.com/thersanchez/aoc/2017/day06a/mem"
-
 // States is a collection of memory hashes and steps requiered to get to that memory state.
 type States struct {
 	data map[string]int
+}
+
+// Memory contains a hash.
+type Memory interface {
+	Hash() string
 }
 
 // NewStates returns a new empty States ready to use.
@@ -15,13 +18,13 @@ func NewStates() *States {
 }
 
 // Add adds the memory hash of m to s.
-func (s *States) Add(m mem.Mem, step int) {
+func (s *States) Add(m Memory, step int) {
 	s.data[m.Hash()] = step
 }
 
 // Has returns the step associated with a given memory
 // or false if not found.
-func (s *States) Has(m mem.Mem) (int, bool) {
+func (s *States) Has(m Memory) (int, bool) {
 	step, ok := s.data[m.Hash()]
 	return step, ok
 }
