8959cda24a9797eba494c36358ae75c54becb270
Esther Sánchez <sanchez.ther@gmail.com>
2021-01-30 18:20:25 +0100

    2017 day07a make Node fields private

--- 2017/day07a/node.go
+++ 2017/day07a/node.go
@@ -2,14 +2,22 @@ package main
 
 // Node represents a program.
 type Node struct {
-	ID       string
-	Weight   int
-	Parent   *Node
+	id       string
+	weight   int
+	parent   *Node
 	children map[string]*Node
+}
+
+// NewNode return a new node with the given id and weight.
+func NewNode(id string, weight int) *Node {
+	return &Node{
+		id:     id,
+		weight: weight,
+	}
 }
 
 // AddChildren adds or update a child of n.
 func (n *Node) AddChildren(child *Node) {
-	n.children[child.ID] = child
-	child.Parent = n
+	n.children[child.id] = child
+	child.parent = n
 }
