2be2e400b955cbc911357512f4f6f740b5a6a460
Esther Sánchez <sanchez.ther@gmail.com>
2021-06-13 14:47:30 +0200

    2017 day07a main parses all lines in input.txt

--- 2017/day07a/main.go
+++ 2017/day07a/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 func main() {
+	r, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		name, weight, children, err := ParseLine(scanner.Text())
+		if err != nil {
+			log.Fatalf("error parsing line: %v", err)
+		}
+
+		_, _, _ = name, weight, children
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("failure scanning input file: %v", err)
+	}
 }
