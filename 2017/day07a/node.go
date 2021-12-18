9d2a8d66517a436f8fe54d6401eb5b04d6fd14ac
Esther Sánchez <sanchez.ther@gmail.com>
2021-12-18 21:15:46 +0100

    2017 day07a expose node fields

--- 2017/day07a/node.go
+++ 2017/day07a/node.go
@@ -2,22 +2,22 @@ package day07a
 
 // Node represents a program.
 type Node struct {
-	id       string
-	weight   int
-	parent   *Node
-	children map[string]*Node
+	ID       string
+	Weight   int
+	Parent   *Node
+	Children map[string]*Node
 }
 
 // NewNode return a new node with the given id and weight.
 func NewNode(id string, weight int) *Node {
 	return &Node{
-		id:     id,
-		weight: weight,
+		ID:     id,
+		Weight: weight,
 	}
 }
 
 // AddChildren adds or update a child of n.
 func (n *Node) AddChildren(child *Node) {
-	n.children[child.id] = child
-	child.parent = n
+	n.Children[child.ID] = child
+	child.Parent = n
 }
