1104ba1a5efef181c06dd3fd4fd7fc95d273a6a1
Esther Sánchez <sanchez.ther@gmail.com>
2021-12-21 18:32:38 +0000

    2017 day07b wip node_tests

--- 2017/day07b/node.go
+++ 2017/day07b/node.go
@@ -1,8 +1,37 @@
 package day07b
 
-import "github.com/thersanchez/aoc/2017/day07a"
-
 type Node struct {
-	day07a.Node
-	TotalWeight int
+	id          string
+	weight      int
+	parent      *Node
+	children    map[string]*Node
+	totalWeight int
+}
+
+// CalculateTotalWeight calculates the total weight of a node, this is, its weight plus the total weight of its children.
+// This function requires that the total weight of the childen has been previously calculated.
+func (n *Node) CalculateTotalWeight() {
+	n.totalWeight = n.weight
+	for _, c := range n.children {
+		n.totalWeight += c.totalWeight
+	}
+}
+
+// GetTotalWeight returns the total weight of the node. It only returns correct values if CalculateTotalWeight have been called previously.
+func (n *Node) GetTotalWeight() int {
+	return n.totalWeight
+}
+
+// NewNode return a new node with the given id and weight.
+func NewNode(id string, weight int) *Node {
+	return &Node{
+		id:     id,
+		weight: weight,
+	}
+}
+
+// AddChildren adds or update a child of n.
+func (n *Node) AddChildren(child *Node) {
+	n.children[child.id] = child
+	child.parent = n
 }
---
+++ 2017/day07b/node_test.go
@@ -0,0 +1,14 @@
+package day07b_test
+
+import "testing"
+
+// TODO:
+// create node, calculateTotalWeight, GetTotalWeight
+// same for rest of nodes but with AddChildren
+func TestCalculateTotalWeight_OneNode(t *testing.T) {
+
+}
+
+func TestCalculateTotalWeight_OneParentOneChildren(t *testing.T) {
+
+}
