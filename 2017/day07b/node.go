0e723bf84b0a9f39c525c07aa4f2eb2cb8038f37
Esther Sánchez <sanchez.ther@gmail.com>
2022-01-09 19:28:48 +0000

    2017 day07b node completed

--- 2017/day07b/node.go
+++ 2017/day07b/node.go
@@ -1,37 +1,64 @@
 package day07b
 
+import (
+	"fmt"
+)
+
 type Node struct {
-	id          string
-	weight      int
-	parent      *Node
-	children    map[string]*Node
+	id       string
+	weight   int
+	parent   *Node
+	children map[string]*Node
+	// totalWeight is the weight of the node plus the total weight of its children.
+	// The zero value means it has not yet been calculated. See TotalWeight.
 	totalWeight int
 }
 
-// CalculateTotalWeight calculates the total weight of a node, this is, its weight plus the total weight of its children.
-// This function requires that the total weight of the childen has been previously calculated.
-func (n *Node) CalculateTotalWeight() {
-	n.totalWeight = n.weight
-	for _, c := range n.children {
-		n.totalWeight += c.totalWeight
-	}
-}
-
-// GetTotalWeight returns the total weight of the node. It only returns correct values if CalculateTotalWeight have been called previously.
-func (n *Node) GetTotalWeight() int {
-	return n.totalWeight
-}
-
 // NewNode return a new node with the given id and weight.
-func NewNode(id string, weight int) *Node {
-	return &Node{
-		id:     id,
-		weight: weight,
+func NewNode(id string, weight int) (*Node, error) {
+	if id == "" {
+		return nil, fmt.Errorf("invalid id (%q), empty string", id)
 	}
+
+	if weight < 1 {
+		return nil, fmt.Errorf("invalid weight (%d), should be > 0", weight)
+	}
+
+	return &Node{
+		id:       id,
+		weight:   weight,
+		children: make(map[string]*Node),
+	}, nil
+}
+
+// Id returns the id of the node.
+func (n *Node) Id() string {
+	return n.id
 }
 
 // AddChildren adds or update a child of n.
 func (n *Node) AddChildren(child *Node) {
 	n.children[child.id] = child
 	child.parent = n
+	n.invalidateTotalWeight()
+}
+
+func (n *Node) invalidateTotalWeight() {
+	n.totalWeight = 0
+	if n.parent != nil {
+		n.parent.invalidateTotalWeight()
+	}
+}
+
+// TotalWeight returns the total weight of a node, this is, its weight plus the total weight of its children.
+func (n *Node) TotalWeight() int {
+	if n.totalWeight != 0 {
+		return n.totalWeight
+	}
+
+	n.totalWeight = n.weight
+	for _, c := range n.children {
+		n.totalWeight += c.TotalWeight()
+	}
+	return n.totalWeight
 }
--- 2017/day07b/node_test.go
+++ 2017/day07b/node_test.go
@@ -1,14 +1,120 @@
 package day07b_test
 
-import "testing"
+import (
+	"strconv"
+	"testing"
 
-// TODO:
-// create node, calculateTotalWeight, GetTotalWeight
-// same for rest of nodes but with AddChildren
-func TestCalculateTotalWeight_OneNode(t *testing.T) {
+	"github.com/thersanchez/aoc/2017/day07b"
+)
 
+func TestNodeId(t *testing.T) {
+	t.Parallel()
+
+	want := "some_id"
+	n, err := day07b.NewNode(want, 42)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	got := n.Id()
+	if want != got {
+		t.Errorf("wrong id, want %q, got %q", want, got)
+	}
 }
 
-func TestCalculateTotalWeight_OneParentOneChildren(t *testing.T) {
+func TestNonPositiveWeightAreRejected(t *testing.T) {
+	t.Parallel()
 
+	weights := []int{0, -1, -42}
+
+	for _, w := range weights {
+		w := w
+		t.Run(strconv.Itoa(w), func(t *testing.T) {
+			t.Parallel()
+
+			_, err := day07b.NewNode("id", w)
+			if err == nil {
+				t.Errorf("unexpected success")
+			}
+		})
+	}
+}
+
+func TestEmptyId(t *testing.T) {
+	t.Parallel()
+
+	_, err := day07b.NewNode("", 42)
+	if err == nil {
+		t.Errorf("unexpected success")
+	}
+}
+
+func TestTotalWeight_OneNode(t *testing.T) {
+	want := 42
+	n, err := day07b.NewNode("some_id", want)
+	if err != nil {
+		t.Fatalf("failed to create node: %v", err)
+	}
+
+	got := n.TotalWeight()
+
+	if got != want {
+		t.Errorf("wrong weight, want %d, got %d", want, got)
+	}
+}
+
+func TestTotalWeight_OneParentOneChildren(t *testing.T) {
+	pWeight := 42
+	cWeight := 13
+	want := pWeight + cWeight
+
+	p, err := day07b.NewNode("parent", pWeight)
+	if err != nil {
+		t.Fatalf("failed to create parent node: %v", err)
+	}
+
+	c, err := day07b.NewNode("child", cWeight)
+	if err != nil {
+		t.Fatalf("failed to create child node: %v", err)
+	}
+
+	p.AddChildren(c)
+
+	got := p.TotalWeight()
+
+	if got != want {
+		t.Errorf("wrong weight, want %d, got %d", want, got)
+	}
+}
+
+func TestTotalWeight_BigTree(t *testing.T) {
+	createNode := func(w int) *day07b.Node {
+		id := strconv.Itoa(w)
+		node, err := day07b.NewNode(id, w)
+		if err != nil {
+			t.Fatalf("failed to create node %s: %v", id, err)
+		}
+		return node
+	}
+
+	n1 := createNode(1)
+	n2 := createNode(2)
+	n3 := createNode(3)
+	n4 := createNode(4)
+	n5 := createNode(5)
+	n6 := createNode(6)
+	n7 := createNode(7)
+
+	n3.AddChildren(n7)
+	n2.AddChildren(n4)
+	n2.AddChildren(n5)
+	n2.AddChildren(n6)
+	n1.AddChildren(n2)
+	n1.AddChildren(n3)
+
+	got := n1.TotalWeight()
+	want := 28
+	if got != want {
+		t.Errorf("wrong weight, want %d, got %d", want, got)
+	}
 }
